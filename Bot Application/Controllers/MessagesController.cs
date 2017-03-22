e78f76968a74842b460d82f89e00bce84a1b100b
Guy Levy <Guy Levy>
2017-03-22 14:21:36 +0100

    Add project files.

---
+++ Bot Application/Controllers/MessagesController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.Bot.Builder.Dialogs;
+using Bot_Application.Dialogs;
+
+namespace Bot_Application
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        //public async Task<HttpResponseMessage> Post([FromBody]Activity message)
+        //{
+        //    if (message.Type == ActivityTypes.Message)
+        //    {
+        //        ConnectorClient connector = new ConnectorClient(new Uri(message.ServiceUrl));
+        //        Activity reply;
+                
+        //            // calculate something for us to return
+        //            int length = (message.Text ?? string.Empty).Length;
+        //            // return our reply to the user
+        //            reply = message.CreateReply($"You sent '{message.Text}' which was {length} characters");
+        //            await connector.Conversations.ReplyToActivityAsync(reply);
+
+                
+        //    }
+        //    else
+        //    {
+        //        HandleSystemMessage(message);
+        //    }
+        //    var response = Request.CreateResponse(HttpStatusCode.OK);
+        //    return response;
+        //}
+
+        public virtual async Task<HttpResponseMessage> Post([FromBody] Activity activity)
+        {
+            // check if activity is of type message
+            if (activity != null && activity.GetActivityType() == ActivityTypes.Message)
+            {
+                await Conversation.SendAsync(activity, () => new SimpleMathDialog());
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            return new HttpResponseMessage(System.Net.HttpStatusCode.Accepted);
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+
+
+}
---
+++ Bot Application/Dialogs/SimpleMathDialog.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+
+namespace Bot_Application.Dialogs
+{
+    [LuisModel("bb3f9f1a-2e53-4841-9c60-a5a24ddf3b9d", "b906bcbff8604806a5f19401ca25a3bf")]
+    [Serializable]
+    public class SimpleMathDialog : LuisDialog<object>
+    {
+        private bool TryFindNumbers(LuisResult result, out IEnumerable<double> numbers)
+        {
+            numbers = result.Entities.Where(entity => entity.Type == "builtin.number").Select(entity => { Double.TryParse(entity.Entity, out double entityAsDouble); return entityAsDouble; });
+            if (numbers != null && numbers.Count() > 0)
+            {
+                return true;
+            }
+            else
+            {
+                numbers = new List<double>();
+                return false;
+            }
+        }
+
+        [LuisIntent("")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            string message = $"Sorry I did not understand: " + string.Join(", ", result.Intents.Select(i => i.Intent));
+            await context.PostAsync(message);
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("sum numbers")]
+        public async Task Sum(IDialogContext context, LuisResult result)
+        {
+            if (TryFindNumbers(result, out IEnumerable<double> numbersToSum))
+            {
+                string reply = string.Format("sum is {0}", numbersToSum.Sum());
+                await context.PostAsync(reply);
+            }
+            else
+            {
+                await context.PostAsync("did not find numbers to sum");
+            }
+            context.Wait(MessageReceived);
+        }
+      
+        public SimpleMathDialog()
+        {
+        }
+
+
+        public SimpleMathDialog(ILuisService service)
+            : base(service)
+        {
+        }     
+       
+    }
+}
